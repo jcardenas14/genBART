a12db1403212c4218b969932555ecdee4fd0267f
jcardenas14 <jcardenas256@gmail.com>
2017-05-19 12:15:42 -0500

    add startup message

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage('Welcome to genBART. To get started, I recommend walking 
+through the vignette. It can be accessed by browseVignettes("genBART").')
+}

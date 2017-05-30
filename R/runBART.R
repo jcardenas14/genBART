96f10b12497f3c24653e6d7a34be7becdbba5074
jcardenas14 <jcardenas256@gmail.com>
2017-05-30 10:18:17 -0500

    runGitHub

--- R/runBART.R
+++ R/runBART.R
@@ -6,10 +6,11 @@
 #' @import ggplot2
 #' @export
 runBart <- function() {
-  appDir <- system.file("shiny-app", "BART", package = "genBART")
-  if (appDir == "") {
-    stop("Could not find BART directory. Try re-installing `genBART`.", 
-         call. = FALSE)
-  }
-  shiny::runApp(appDir, display.mode = "normal")
+  #appDir <- system.file("shiny-app", "BART", package = "genBART")
+  #if (appDir == "") {
+    #stop("Could not find BART directory. Try re-installing `genBART`.", 
+         #call. = FALSE)
+  #}
+  #shiny::runApp(appDir, display.mode = "normal")
+  shiny::runGitHub("BART_app", "jcardenas14")
 }

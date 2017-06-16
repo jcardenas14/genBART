afbc30e1a8e94e8158ae30371157e9b548dd9ad1
jcardenas14 <jcardenas256@gmail.com>
2017-06-16 15:41:58 -0500

    add genDendrograms unit tests

--- tests/testthat/test_genDendros.R
+++ tests/testthat/test_genDendros.R
@@ -1,15 +1,8 @@
 library(testthat)
 library(genBART)
 
-design.base <- tb.design[which(tb.design$timepoint == 0),]
-expr.base <- tb.expr[,which(tb.design$timepoint == 0)]
-
-des.info <- desInfo(y = expr.base, design = design.base, columnname = "columnname")
-genDendrograms2(des.info)
-
-
-
-test_that("baseline data with no controls outputs correct dendrogram", {
+context("cross-sectional data without controls")
+test_that("cross-sectional data with no controls outputs correct dendrogram", {
   design.base <- tb.design[which(tb.design$timepoint == 0),]
   expr.base <- tb.expr[1:100,which(tb.design$timepoint == 0)]
   base.means <- apply(expr.base, 1, mean, na.rm = TRUE)
@@ -17,7 +10,7 @@ test_that("baseline data with no controls outputs correct dendrogram", {
   h1_test_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(expr.base.norm))))
   h1_test_rowdendro <- as.dendrogram(fastcluster::hclust(dist(expr.base.norm)))
   des.info <- desInfo(y = expr.base, design = design.base, columnname = "columnname")
-  dends <- genDendrograms2(des.info)
+  dends <- genDendrograms(des.info)
   expect_equal(dends$h1b_coldendro, NULL)
   expect_equal(dends$h1b_rowdendro, NULL)
   expect_equal(dends$h2b_coldendro, NULL)
@@ -30,7 +23,8 @@ test_that("baseline data with no controls outputs correct dendrogram", {
   expect_equal(dends$h1_coldendro, h1_test_coldendro)
 })
 
-test_that("baseline data with controls outputs correct dendrograms", {
+context("cross-sectional data with controls")
+test_that("cross-sectional data with controls outputs correct dendrograms", {
   design.base <- tb.design[which(tb.design$timepoint == 0 & tb.design$clinical_status == "Active"),]
   expr.base <- tb.expr[1:100,which(tb.design$timepoint == 0 & tb.design$clinical_status == "Active")]
   design.hc <- tb.design[which(tb.design$timepoint == 0 & tb.design$clinical_status == "Latent"),]
@@ -51,7 +45,7 @@ test_that("baseline data with controls outputs correct dendrograms", {
   h2_test_rowdendro <- as.dendrogram(fastcluster::hclust(dist(expr.all.hcnorm)))
   des.info <- desInfo(y = expr.all, design = design.all, columnname = "columnname",
                       control_var = "clinical_status", control_val = "Latent")
-  dends <- genDendrograms2(des.info)
+  dends <- genDendrograms(des.info)
   expect_equal(dends$h2b_coldendro, NULL)
   expect_equal(dends$h2b_rowdendro, NULL)
   expect_equal(dends$h3_coldendro, NULL)
@@ -64,6 +58,7 @@ test_that("baseline data with controls outputs correct dendrograms", {
   expect_equal(dends$h2_coldendro, h2_test_coldendro)
 })
 
+context("longitudinal data without controls")
 test_that("longitudinal data with no controls outputs correct dendrograms", {
   design.all <- tb.design[-c(1,13,22),]
   expr.all <- tb.expr[1:100,-c(1,13,22)]
@@ -93,7 +88,7 @@ test_that("longitudinal data with no controls outputs correct dendrograms", {
   des.info <- desInfo(y = expr.all, design = design.all, 
                       columnname = "columnname",sample_id = "sample_id",patient_id = "monkey_id",
                       long = TRUE,time_var = "timepoint", baseline_var = "timepoint", baseline_val = 0)
-  dends <- genDendrograms2(des.info)
+  dends <- genDendrograms(des.info)
   expect_equal(dends$h2b_coldendro, NULL)
   expect_equal(dends$h2b_rowdendro, NULL)
   expect_equal(dends$h2_coldendro, NULL)
@@ -106,13 +101,17 @@ test_that("longitudinal data with no controls outputs correct dendrograms", {
   expect_equal(dends$h3_coldendro, h3_test_coldendro)
 })
 
+context("longitudinal data with controls")
 test_that("longitudinal data with controls outputs correct dendrograms", {
-  design.base <- tb.design[-c(4,19,25,which(tb.design$timepoint != 0 | tb.design$clinical_status != "Active")),]
-  expr.base <- tb.expr[1:100,-c(4,19,25,which(tb.design$timepoint != 0 | tb.design$clinical_status != "Active"))]
-  design.hc <- tb.design[-c(4,19,25,which(tb.design$timepoint != 0 | tb.design$clinical_status != "Latent")),]
-  expr.hc <- tb.expr[1:100,-c(4,19,25,which(tb.design$timepoint != 0 | tb.design$clinical_status != "Latent"))]
+  tb.design2 <- tb.design
+  tb.design2$clinical_status <- as.character(tb.design2$clinical_status)
+  tb.design2$clinical_status[which(tb.design2$clinical_status == "Latent" & tb.design2$timepoint == 0)] <- "HC"
+  design.base <- tb.design2[-c(4,19,25,which(tb.design2$timepoint != 0 | tb.design2$clinical_status != "Active")),]
+  expr.base <- tb.expr[1:100,-c(4,19,25,which(tb.design2$timepoint != 0 | tb.design2$clinical_status != "Active"))]
+  design.hc <- tb.design2[-c(4,19,25,which(tb.design2$clinical_status != "HC")),]
+  expr.hc <- tb.expr[1:100,-c(4,19,25,which(tb.design2$clinical_status != "HC"))]
   
-  design.all <- tb.design[-c(4,19,25),]
+  design.all <- tb.design2[-c(4,19,25),]
   expr.all <- tb.expr[1:100,-c(4,19,25)]
   means.all <- apply(expr.all, 1, mean, na.rm = TRUE)
   expr.all.norm <- expr.all - means.all
@@ -121,12 +120,12 @@ test_that("longitudinal data with controls outputs correct dendrograms", {
   
   means.hc <- apply(expr.hc, 1, mean, na.rm = TRUE)
   expr.hc.all <- cbind(expr.hc, expr.all[,-which(colnames(expr.all) %in% design.hc$columnname)])
-  expr.all.hcnorm <- expr.all - means.hc
+  expr.all.hcnorm <- expr.hc.all - means.hc
   h2_test_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(expr.all.hcnorm))))
   h2_test_rowdendro <- as.dendrogram(fastcluster::hclust(dist(expr.all.hcnorm)))
   
-  design.sub <- tb.design[-c(c(4:6,19:21,25:27), which(tb.design$clinical_status == "Latent")),]
-  expr.sub <- tb.expr[1:100,-c(c(4:6,19:21,25:27), which(tb.design$clinical_status == "Latent"))]
+  design.sub <- tb.design2[-c(c(4:6,19:21,25:27), which(tb.design2$clinical_status == "Latent")),]
+  expr.sub <- tb.expr[1:100,-c(c(4:6,19:21,25:27), which(tb.design2$clinical_status == "Latent"))]
   expr.sub.norm <- expr.sub
   for(i in 1:length(unique(design.sub$monkey_id))){
     index <- which(design.sub$monkey_id %in% unique(design.sub$monkey_id)[i])
@@ -147,11 +146,11 @@ test_that("longitudinal data with controls outputs correct dendrograms", {
   h2b_test_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(expr.base.hcnorm))))
   h2b_test_rowdendro <- as.dendrogram(fastcluster::hclust(dist(expr.base.hcnorm)))
   
-  des.info <- desInfo(y = expr.hc.all, design = design.all, 
+  des.info <- desInfo(y = expr.all, design = design.all, 
                       columnname = "columnname",sample_id = "sample_id",patient_id = "monkey_id",
                       long = TRUE,time_var = "timepoint", baseline_var = "timepoint", baseline_val = 0,
-                      control_var = "clinical_status", control_val = "Latent")
-  dends <- genDendrograms2(des.info)
+                      control_var = "clinical_status", control_val = "HC")
+  dends <- genDendrograms(des.info)
   expect_equal(dends$h2b_coldendro, h2b_test_coldendro)
   expect_equal(dends$h2b_rowdendro, h2b_test_rowdendro)
   expect_equal(dends$h2_coldendro, h2_test_coldendro)
@@ -162,157 +161,32 @@ test_that("longitudinal data with controls outputs correct dendrograms", {
   expect_equal(dends$h1_coldendro, h1_test_coldendro)
   expect_equal(dends$h3_rowdendro, h3_test_rowdendro)
   expect_equal(dends$h3_coldendro, h3_test_coldendro)
-  
-  
 })
 
-design_info <- des.info
-genDendrograms2 <- function(design_info) {
-  hc <- design_info$hc
-  long <- design_info$long
-  patient_id <- design_info$patient_id
-  baseline_var <- design_info$baseline_var
-  baseline_val <- design_info$baseline_val
-  control_var <- design_info$control_var
-  control_val <- design_info$control_val
-  design <- design_info$design
-  final_expression <- design_info$y
-  PROBE_ID <- SYMBOL <- rownames(final_expression)
-  final_expression <- cbind(PROBE_ID = PROBE_ID, SYMBOL = SYMBOL,
-                            final_expression)
-  print("Normalizing Expression and Clustering....")
-  y1 <- dataManipulate(y = final_expression, x = design, 
-                       colname = "columnname", format = "Probes",
-                       allsamples = TRUE)
-  print("Clustering All Samples Median Normalized Heatmap")
-  h1_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(y1$heatexp))))
-  h1_rowdendro <- as.dendrogram(fastcluster::hclust(dist(y1$heatexp)))
-  if (long) {
-    if (hc) {
-      y2 <- dataManipulate(y = final_expression, x = design,
-                           colname = "columnname", ref_var = control_var,
-                           ref_level = control_val, long = FALSE, keep = TRUE,
-                           format = "Probes", allsamples = FALSE)
-      print("Clustering All Samples Healthy Normalized Heatmap")
-      h2_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(y2$heatexp))))
-      h2_rowdendro <- as.dendrogram(fastcluster::hclust(dist(y2$heatexp)))
-    } else {
-      h2_coldendro <- NULL
-      h2_rowdendro <- NULL
-    }
-    if (!is.null(baseline_var) & !is.null(baseline_val)) {
-      base_sample_id <- design$columnname[design[, baseline_var] == baseline_val
-                                          ]
-      if (hc) {
-        base_sample_id <- design$columnname[which(design[, baseline_var] == 
-                                                    baseline_val &
-                                                    design[, control_var] != 
-                                                    control_val)]
-      }
-      ind1 <- which(colnames(final_expression) %in% c("PROBE_ID", "SYMBOL"))
-      ind2 <- which(colnames(final_expression) %in% base_sample_id)
-      exp_base <- final_expression[, c(ind1, ind2)]
-      des_base <- design[which(design$columnname %in% colnames(exp_base)), ]
-      y1b <- dataManipulate(y = exp_base, x = des_base, colname = "columnname",
-                            format = "Probes", allsamples = TRUE)
-      print("Clustering Baseline Median Normalized Heatmap")
-      h1b_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(y1b$heatexp))))
-      h1b_rowdendro <- as.dendrogram(fastcluster::hclust(dist(y1b$heatexp)))
-      if (hc) {
-        base_sample_id <- design$columnname[which(design[, baseline_var] == 
-                                                    baseline_val &
-                                                    design[, control_var] == 
-                                                    control_val)]
-        ind1 <- which(colnames(final_expression) %in% c("PROBE_ID", "SYMBOL"))
-        ind2 <- which(colnames(final_expression) %in% base_sample_id)
-        exp_base <- final_expression[, c(ind1, ind2)]
-        des_base <- design[which(design$columnname %in% colnames(exp_base)), ]
-        y2b <- dataManipulate(y = exp_base, x = des_base, 
-                              colname = "columnname", ref_var = control_var, 
-                              ref_level = control_val, long = FALSE, 
-                              keep = TRUE, format = "Probes", allsamples = FALSE
-                              )
-        print("Clustering Baseline Healthy Normalized Heatmap")
-        h2b_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(y2b$heatexp)))
-        )
-        h2b_rowdendro <- as.dendrogram(fastcluster::hclust(dist(y2b$heatexp)))
-        des_wo_controls <- design[-which(design[, control_var] == control_val), 
-                                  ]
-        h5index <- c(1, 2, which(colnames(final_expression) %in%
-                                   des_wo_controls$columnname))
-        y3 <- dataManipulate(y = final_expression[, h5index], 
-                             x = des_wo_controls, colname = "columnname", 
-                             ref_var = baseline_var, ref_level = baseline_val, 
-                             long = TRUE, subjects = patient_id, keep = FALSE,
-                             format = "Probes", allsamples = FALSE)
-        print("Clustering All Samples Baseline Normalized Heatmap")
-        h3_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(y3$heatexp))))
-        h3_rowdendro <- as.dendrogram(fastcluster::hclust(dist(y3$heatexp)))
-      } else {
-        h2b_coldendro <- NULL
-        h2b_rowdendro <- NULL
-        y3 <- dataManipulate(y = final_expression, x = design,
-                             colname = "columnname", ref_var = baseline_var,
-                             ref_level = baseline_val, long = TRUE,
-                             subjects = patient_id, keep = FALSE,
-                             format = "Probes", allsamples = FALSE)
-        print("Clustering All Samples Baseline Normalized Heatmap")
-        h3_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(y3$heatexp))))
-        h3_rowdendro <- as.dendrogram(fastcluster::hclust(dist(y3$heatexp)))
-      }
-    } else {
-      h1b_coldendro <- NULL
-      h1b_rowdendro <- NULL
-      h3_coldendro <- NULL
-      h3_rowdendro <- NULL
-      if (!hc) {
-        message("baseline_var and/or baseline_val is unspecified. Cannot produce
-baseline median normalized or all samples baseline normalized data.")
-      } else {
-        message("baseline_var and/or baseline_val is unspecified. Cannot produce
+context("messages")
+test_that("correct messages are thrown",{
+  suppressWarnings(des.info <- desInfo(y = tb.expr[1:100,], design = tb.design, columnname = "columnname",sample_id = "sample_id",
+                      patient_id = "monkey_id",long = TRUE,time_var = "timepoint",control_var = "clinical_status", control_val = "Latent"))
+  expect_message(genDendrograms(des.info), "baseline_var and/or baseline_val is unspecified. Cannot produce
 baseline median normalized, baseline healthy normalized, or all
 samples baseline normalized data.")
-      }
-    }
-  } else {
-    h1b_coldendro <- NULL
-    h1b_rowdendro <- NULL
-    h2b_coldendro <- NULL
-    h2b_rowdendro <- NULL
-    h3_coldendro <- NULL
-    h3_rowdendro <- NULL
-    if (hc) {
-      y2 <- dataManipulate(y = final_expression, x = design,
-                           colname = "columnname", ref_var = control_var,
-                           ref_level = control_val, long = FALSE, keep = TRUE,
-                           format = "Probes", allsamples = FALSE)
-      print("Clustering All Samples Healthy Normalized Heatmap")
-      h2_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(y2$heatexp))))
-      h2_rowdendro <- as.dendrogram(fastcluster::hclust(dist(y2$heatexp)))
-      des_wo_controls <- design[-which(design[, control_var] == control_val), ]
-      h5index <- c(1, 2, which(colnames(final_expression) %in%
-                                 des_wo_controls$columnname))
-      y1b <- dataManipulate(y = final_expression[, h5index], 
-                            x = des_wo_controls, colname = "columnname",
-                            format = "Probes", allsamples = TRUE)
-      print("Clustering Non-Healthy Samples Median Normalized Heatmap")
-      h1b_coldendro <- as.dendrogram(fastcluster::hclust(dist(t(y1b$heatexp))))
-      h1b_rowdendro <- as.dendrogram(fastcluster::hclust(dist(y1b$heatexp)))
-    } else {
-      h2_coldendro <- NULL
-      h2_rowdendro <- NULL
-    }
-  }
-  z <- list(h1b_coldendro = h1b_coldendro, h1b_rowdendro = h1b_rowdendro,
-            h2b_coldendro = h2b_coldendro, h2b_rowdendro = h2b_rowdendro,
-            h1_coldendro = h1_coldendro, h1_rowdendro = h1_rowdendro,
-            h2_coldendro = h2_coldendro, h2_rowdendro = h2_rowdendro,
-            h3_coldendro = h3_coldendro, h3_rowdendro = h3_rowdendro)
-  return(z)
-}
-  
-  
-  
+  suppressWarnings(des.info <- desInfo(y = tb.expr[1:100,], design = tb.design, columnname = "columnname",sample_id = "sample_id",
+                      patient_id = "monkey_id",long = TRUE,time_var = "timepoint"))
+  expect_message(genDendrograms(des.info), "baseline_var and/or baseline_val is unspecified. Cannot produce
+baseline median normalized or all samples baseline normalized data.")
+  des.info <- desInfo(y = tb.expr[1:100,], design = tb.design, columnname = "columnname",sample_id = "sample_id",
+                      long = TRUE,time_var = "timepoint",baseline_var = "timepoint",baseline_val = 0,
+                      control_var = "clinical_status",control_val = "Latent")
+  expect_message(genDendrograms(des.info), "patient_id is not defined. Cannot produce baseline normalized 
+data.")
+  des.info <- desInfo(y = tb.expr[1:100,], design = tb.design, columnname = "columnname",sample_id = "sample_id",
+                      long = TRUE,time_var = "timepoint",baseline_var = "timepoint",baseline_val = 0)
+  expect_message(genDendrograms(des.info), "patient_id is not defined. Cannot produce baseline normalized 
+data.")
+})
+
+
+
   
   
   

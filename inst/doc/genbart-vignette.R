a863f07888b9bc1dfb5652896f5d86b5fc9c8d9f
jcardenas14 <jcardenas256@gmail.com>
2017-12-29 17:49:31 -0600

    Update vignette

--- inst/doc/genbart-vignette.R
+++ inst/doc/genbart-vignette.R
@@ -20,9 +20,21 @@ meta.flow <- metaData(y = tb.flow, design = tb.flow.des, data.type = "flow",
 
 ## ------------------------------------------------------------------------
 norm.data <- normalizeData(meta = meta, norm.method = "mean")
-cluster.data <- clusterData(norm.data = norm.data, dist.method = "euclidean", agg.method = "complete")
+cluster.data <- clusterData(norm.data = norm.data, dist.method = "euclidean", 
+                            agg.method = "complete")
 
 ## ------------------------------------------------------------------------
+# Form clusters
+tb.expr.scale <- data.frame(t(scale(t(tb.expr)))) # center and scale probes
+hc <- fastcluster::hclust(dist(tb.expr.scale))
+cls <- cutree(hc, 10)
+clusters <- list()
+for(i in 1:10){
+  clusters[[i]] <- names(cls)[which(cls == i)]
+  names(clusters)[i] <- paste0("C",i)
+}
+
+# Generate module scores
 mod.scores <- genModScores(meta = meta, gene.sets = clusters, sd.lim = 2)
 
 ## ------------------------------------------------------------------------
@@ -38,7 +50,8 @@ colnames(design2) <- levels(grp)
 dupcor <- duplicateCorrelation(tb.expr, design2, block = tb.design$monkey_id)
 
 ## ------------------------------------------------------------------------
-fit <- lmFit(tb.expr, design2, block = tb.design$monkey_id, correlation = dupcor$consensus.correlation)
+fit <- lmFit(tb.expr, design2, block = tb.design$monkey_id, 
+             correlation = dupcor$consensus.correlation)
 contrasts <- makeContrasts(A_20vsPre = Active20-Active0, A_42vsPre = Active42-Active0,
                            L_20vsPre = Latent20-Latent0, L_42vsPre = Latent42-Latent0,
                            levels=design2)
@@ -52,11 +65,12 @@ grp <- factor(tb.flow.des$Group)
 
 design2 <- model.matrix(~0+grp)
 colnames(design2) <- levels(grp)
-dupcor <- duplicateCorrelation(tb.flow, design2, block = tb.flow.des$monkey_id)
+tb.flow.l2 <- log2(tb.flow + 1) # log2 transform
+dupcor <- duplicateCorrelation(tb.flow.l2, design2, block = tb.flow.des$monkey_id)
+fit.flow <- lmFit(tb.flow.l2, design2, block = tb.flow.des$monkey_id,
+correlation = dupcor$consensus.correlation)
 
-fit.flow <- lmFit(tb.flow, design2, block = tb.flow.des$monkey_id, correlation = dupcor$consensus.correlation)
-
-# Add more contrasts since we have all timepoints available. Additional contrasts could be added as well.
+# Additional contrasts since all timepoints available. 
 contrasts <- makeContrasts(A_20vsPre = Active20-Active0, A_30vsPre = Active30-Active0,
                            A_42vsPre = Active42-Active0, A_56vsPre = Active56-Active0,
                            L_20vsPre = Latent20-Latent0, L_30vsPre = Latent30-Latent0,
@@ -67,30 +81,36 @@ fit2.flow <- contrasts.fit(fit.flow, contrasts)
 fit2.flow <- eBayes(fit2.flow, trend = FALSE)
 
 ## ------------------------------------------------------------------------
-mod.results <- genModelResults(y = tb.expr, data.type = "microarray", object = fit2, lm.Fit = fit, method = "limma")
-mod.results.flow <- genModelResults(y = tb.flow, data.type = "flow", object = fit2.flow, lm.Fit = fit.flow, method = "limma")
+data(gene.symbols) # gene symbols for Illumina probes
+mod.results <- genModelResults(y = tb.expr, data.type = "microarray", object = fit2, 
+                               lm.Fit = fit, method = "limma", var.symbols = gene.symbols)
+mod.results.flow <- genModelResults(y = tb.flow, data.type = "flow", object = fit2.flow, 
+                                    lm.Fit = fit.flow, method = "limma")
 
 ## ------------------------------------------------------------------------
 qus <- qBart(model.results = mod.results, gene.sets = modules)
 
 ## ------------------------------------------------------------------------
-# Create time variable
-time <- module.as$time
-module.as$time <- NULL
+# Format expression data to align with flow data
+gene.data <- data.frame(t(tb.expr))
+rownames(gene.data) <- paste0(tb.design$monkey_id, "_", tb.design$timepoint)
+flow.data <- data.frame(t(tb.flow))
+flow.data <- flow.data[match(rownames(gene.data), rownames(flow.data), nomatch = 0), ]
+gene.data <- gene.data[match(rownames(flow.data), rownames(gene.data), nomatch = 0), ]
 
-# Format flow data to run correlations and match flow samples with module.as
-flow <- data.frame(t(tb.flow))
-flow <- flow[match(rownames(module.as), rownames(flow), nomatch = 0), ]
+# Create time variable
+time <- tb.flow.des$timepoint[match(rownames(flow.data),tb.flow.des$columnname,nomatch = 0)]
 
 # Run correlations formatted for BART
-corrs <- crossCorr(x = module.as, y = flow, by = time, by.name = "days", 
-                   description = "Mod.Act.Score vs Flow", x.var = "Mod.Act.Score", 
+corrs <- crossCorr(x = gene.data, y = flow.data, by = time, by.name = "days", 
+                   description = "Genes vs Flow", x.var = "Genes", 
                    y.var = "Flow", method = "spearman")
 
 
 ## ------------------------------------------------------------------------
-genFile(meta = list(meta, meta.flow), module.scores = mod.scores, dendrograms = cluster.data, 
-        model.results = list(mod.results, mod.results.flow), project.name = "BART example")
+genFile(meta = list(meta, meta.flow), module.scores = mod.scores, 
+        dendrograms = cluster.data, model.results = list(mod.results, mod.results.flow), 
+        project.name = "BART example")
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  path <- paste(getwd(), "/", "BART example", sep = "")
